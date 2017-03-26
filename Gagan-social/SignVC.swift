01910f416956f714448e6aaa279091c63e909185
Gagandeeps-Air <you@example.com>
2017-03-26 19:48:47 +0530

    Facebook and firebase authentication incoperated

--- Gagan-social/SignVC.swift
+++ Gagan-social/SignVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
 class SignVC : UIViewController {
 
@@ -20,6 +23,35 @@ class SignVC : UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("Gagan: Unable to authenticate with facebook -\(error)")
+            }else if result?.isCancelled == true {
+                print("Gagan: User cancelled facebook authentication")
+            }else {
+                print("Gagan: Successfully authenticated with facebook")
+                
+                let credential  = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credentail : FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credentail, completion: { (user, error) in
+            if error != nil {
+                print("Gagan: Unable to authenticate with firebase -\(error)")
+            }else {
+                print(" Gagan: Successfully authenticated with firebase")
+            }
+            
+        })
+        
+    }
 }
 
